0fb1bc7abb2afb1431b737fe6670e579c81ed1e0
Kiran024 <145450942+Kiran024@users.noreply.github.com>
2023-11-02 14:20:11 +0530

    initail: commit files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Laptop from "./pages/Laptop";
+import Phone from "./pages/Phone";
+import MNotes from "./components/Notes/MNotes";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Provider} from "./useContext";
+import UseContext from "./useContext";
 
 function App() {
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+  const { selected, setSelected } = UseContext();
+
+  useEffect(() => {
+    setSelected(localStorage.getItem("selected") || "");
+    // eslint-disable-next-line
+  }, [selected]);
+  const checkScreenSize = () => {
+    setScreenSize(window.innerWidth);
+  };
+
+  window.addEventListener("resize", checkScreenSize);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider>
+      <div className="App">
+        {screenSize > 500 ? (
+          <Laptop />
+        ) : (
+          <Router>
+            <Routes>
+              <Route path="/" element={<Phone />} />
+              <Route path="/notes" element={<MNotes />} />
+            </Routes>
+          </Router>
+        )}
+      </div>
+    </Provider>
   );
 }
 
